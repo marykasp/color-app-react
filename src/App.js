fd5733c231ae166e0b94033f62d6977fe8f2460c
marykasp <kasparian.mary@gmail.com>
2022-06-07 15:27:38 -0500

    Add palette component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Palette from "./components/Palette";
+import seedColors from "./seedColors";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* pass in seedColors object properties as props on Palette */}
+      <Palette {...seedColors[0]} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Palette.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import ColorBox from "./ColorBox";
+
+function Palette(props) {
+  // destructure props object
+  // const { paletteName } = props;
+  return (
+    <div className="Palette">
+      {/* Navbar goes here */}
+      <div className="Palette-colors">{/* Color boxes */}</div>
+      {/* Footer */}
+    </div>
+  );
+}
+export default Palette;
