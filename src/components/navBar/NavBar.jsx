dfcbd68727b95030325644f9e17836e84c3bf9d9
marykasp <kasparian.mary@gmail.com>
2022-06-08 13:10:09 -0500

    Add snackbar from material UI

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -2,19 +2,39 @@ import React, { useState } from "react";
 import Slider from "rc-slider";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 import "rc-slider/assets/index.css";
 import "./navbar.css";
 
 function NavBar(props) {
   const [format, setFormat] = useState("hex");
+  const [open, setOpen] = useState(false);
   const { level, changeLevel, changeFormat } = props;
 
   // event handler for select dropdown -will update the format state
-  function handleChange(e) {
+  function handleFormatChange(e) {
     setFormat(e.target.value);
+    setOpen(true);
     // change format passed down from Palette component
     changeFormat(e.target.value);
   }
+
+  function closeSnackbar() {
+    setOpen(false);
+  }
+
+  const action = [
+    <IconButton
+      onClick={closeSnackbar}
+      color="inherit"
+      key="close"
+      arial-label="close"
+    >
+      <CloseIcon />
+    </IconButton>,
+  ];
 
   return (
     <header className="Navbar">
@@ -47,12 +67,25 @@ function NavBar(props) {
         </div>
       </div>
       <div className="select-container">
-        <Select onChange={handleChange} value={format}>
+        <Select onChange={handleFormatChange} value={format}>
           <MenuItem value="hex">HEX - #ffff</MenuItem>
           <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
           <MenuItem value="rgba">RGBA - rgb(255,255,255,1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={open}
+        autoHideDuration={3000}
+        message={
+          <span id="message-id">Format Changed To {format.toUpperCase()}!</span>
+        }
+        ContentProps={{
+          "aria-describedby": "message-id",
+        }}
+        action={action}
+        onClose={closeSnackbar}
+      />
     </header>
   );
 }
