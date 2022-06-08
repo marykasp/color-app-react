8c25dfe594b04123b750f84aa52d3de7f840a703
marykasp <kasparian.mary@gmail.com>
2022-06-07 19:01:39 -0500

    Add event handler to clipboard component to change copied state, use state to change classname of copy-overlay

--- src/components/colorBox/ColorBox.jsx
+++ src/components/colorBox/ColorBox.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./colorbox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ColorBox(props) {
   // destructure props
   const { background, name } = props;
+  const [copied, setCopied] = useState(false);
+
+  // event handler changes the copied state
+  function changeCopyState() {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+    console.log(copied);
+  }
 
   return (
-    <CopyToClipboard text={background}>
+    <CopyToClipboard text={background} onCopy={changeCopyState}>
       <div className="ColorBox" style={{ background: background }}>
+        <div
+          className={`copy-overlay ${copied && "show"}`}
+          style={{ background: background }}
+        ></div>
         <div className="copy-container">
           <div className="box-content">
             <span>{name}</span>
