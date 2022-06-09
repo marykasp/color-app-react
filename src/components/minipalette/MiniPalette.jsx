df46dbd31a2e3b917ab9cbc398fc36ee85d73f01
marykasp <kasparian.mary@gmail.com>
2022-06-09 11:14:52 -0500

    Style miniPalette component

--- src/components/minipalette/MiniPalette.jsx
+++ src/components/minipalette/MiniPalette.jsx
@@ -4,12 +4,25 @@ import { Link } from "react-router-dom";
 
 function MiniPalette(props) {
   const { id, paletteName, colors, emoji } = props;
+  // create mini color boxes for each of the colors on the palette - colors is an array of objects -pass in object to callback function use color property to style the background color of the div
+  const miniColorBoxes = colors.map((color) => (
+    <div
+      className="minicolor"
+      style={{ backgroundColor: color.color }}
+      key={color.name}
+    ></div>
+  ));
   return (
     <div className="root">
-      <div className="colors">{/* div for each color */}</div>
-      <h5 className="title">
-        {paletteName} <span className="emoji">{emoji}</span>
-      </h5>
+      <div className="colors">
+        {/* add mini color boxes here */}
+        {miniColorBoxes}
+      </div>
+      <Link to={`palette/${id}`}>
+        <h5 className="title">
+          {paletteName} <span className="emoji">{emoji}</span>
+        </h5>
+      </Link>
     </div>
   );
 }
