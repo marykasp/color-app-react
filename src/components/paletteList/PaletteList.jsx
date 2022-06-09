d3b626ff9b4237a86f6a94b99d214606711c8e5d
marykasp <kasparian.mary@gmail.com>
2022-06-09 10:58:01 -0500

    Start styling PaletteList component

--- src/components/paletteList/PaletteList.jsx
+++ src/components/paletteList/PaletteList.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import MiniPalette from "../minipalette/MiniPalette";
 import "./palettelist.css";
 
@@ -7,12 +6,18 @@ function PaletteList(props) {
   // passed the array of starter palettes down as a prop
   const { palettes } = props;
   return (
-    <div>
-      <h1>React Color Palettes</h1>
-      {/* iterate over the palettes array and create jsx element with each palette object properties */}
-      {palettes.map((palette) => (
-        <MiniPalette {...palette} />
-      ))}
+    <div className="Palette-list">
+      <div className="container">
+        <nav className="nav">
+          <h1>React Color Palettes</h1>
+        </nav>
+        <div className="palettes">
+          {/* iterate over the palettes array and create MiniPalette component with each palette object properties */}
+          {palettes.map((palette) => (
+            <MiniPalette {...palette} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
